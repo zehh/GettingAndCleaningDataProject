1ff2b82a9e3e92d8de8ed10357056b10a63b48ab
zehh <pedrohenriquecaldeira@gmail.com>
2023-02-21 23:55:27 -0300

    Finished the assignment. Haven't found any errors in the testing.

--- cleaningData.R
+++ cleaningData.R
@@ -65,5 +65,60 @@ variablenames<-gsub("^([0-9]+)|(\\s+)","",variablenames)
 variablenameslist<-list()
 variablenameslist[[2]]<-variablenames
 
-tidytestmatrix<-matrix(unlist(tidytestset), nrow = nrows, ncol = ncols, byrow = TRUE,
-                       dimnames = variablenameslist)
+tidytestmatrix<-matrix(unlist(tidytestset), nrow = nrows, ncol = ncols,
+                       byrow = TRUE, dimnames = variablenameslist)
+
+# the same that was done above now needs to be done to create the training data
+nrowstraining <- length(trainingset)
+tidytrainingset <- tidyObs(trainingset)
+## number of variables is the same (same variables)
+tidytrainingmatrix<- matrix(unlist(tidytrainingset),
+                            nrow = nrowstraining, ncol = ncols, byrow = TRUE,
+                            dimnames = variablenameslist)
+
+## now that both the data sets have been created, we need to add the
+## activities and subjects to the observations contained in them
+
+## create activity labels
+activity_labels <- read_lines("./data/UCI HAR Dataset/activity_labels.txt") %>%
+        substring(3) %>% unique() %>% 
+        { 
+                {. -> tmp} %>% factor(levels = tmp)
+        }
+
+## read the activities and subjects
+activity_test <- read_lines(paste(testdir,sep="/","y_test.txt"))
+activity_training <- read_lines(paste(traindir,sep="/","y_train.txt"))
+subject_test <- read_lines(paste(testdir,sep="/","subject_test.txt"))
+subject_train <- read_lines(paste(traindir,sep="/","subject_train.txt"))
+
+## convert activity to the labels
+activity_test <- factor(activity_test,
+                                levels = 1:length(activity_labels),
+                                labels = activity_labels)
+activity_training <- factor(activity_training,
+                            levels = 1:length(activity_labels),
+                            labels = activity_labels)
+
+## add the activities and subjects to their respective data set
+tidytrainingmatrix %>% as_tibble(.name_repair = "unique") %>% 
+        mutate(activity=activity_training,
+               subject = as.numeric(subject_train),
+               type = rep("TRAIN",times=nrowstraining)) -> tidytrainingtibble
+tidytestmatrix %>% as_tibble(.name_repair = "unique") %>% 
+        mutate(activity=activity_test, subject = as.numeric(subject_test),
+               type = rep("TEST",times=nrows)) -> tidytesttibble
+
+## merge both the tibbles
+merged<-bind_rows(tidytrainingtibble,tidytesttibble)
+
+## select variables of interest as provided by the assignment
+merged%>%select(grep("std|mean\\(|activity|subject",colnames(merged)))-> 
+        mergedselected
+
+## group by the activity and calculate the mean
+mergedselected%>%group_by(subject,activity)%>%
+        summarize(across(everything(),mean, .names = "mean_{.col}")) ->
+        finaldataset
+
+finaldataset
